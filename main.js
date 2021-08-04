83f06db72d902a591f367417052b1736f170b3f3
Chris Kade <chriskade1999@gmail.com>
2021-08-03 21:22:42 -0400

    Main coins section added

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
+
+
 function createRequest(){
     const xhr = new XMLHttpRequest();
     xhr.responseType = 'JSON';
     
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            let json = JSON.parse(xhr.responseText);
+            const json = JSON.parse(xhr.responseText);
+            console.log(json);
+            return json;
 
         }
     }
+    // This API request will return all prices along with other various information about cryptocurrencies.
     xhr.open('get','https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false', true);
     xhr.send();
-    let json = new xhr.responseText;
+    
     //console.log(json);
 }
 
-//const prices = createRequest();
-//console.log(price);
+$(document).ready(function() {
+    let req = createRequest;
+    console.log(req);
+});
