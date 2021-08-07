0a89af0b4edbd94cd196bce2dc4ef7e0f77aa925
Chris Kade <chriskade1999@gmail.com>
2021-08-07 16:53:37 -0400

    Timed display of images for popular coins

--- main.js
+++ main.js
@@ -5,6 +5,25 @@ let formatter = new Intl.NumberFormat('en-US', {
     currency: 'USD'
 });
 
+let images = ['assets/bitcoin.jpg','assets/cardano.jpg','assets/Ethereum-coin.webp'],
+    index = 0,
+    max = images.length - 1;
+
+let timer = setInterval(function() {
+    let currentImg = images[index];
+    index = (index == max) ? 0 : ++index;
+    $('img').attr('src', currentImg);
+}, 2000);
+/*
+$('input').autocomplete({
+    source: ['btc','eth','usdt','bnb','ada','xrp','doge','usdc','dot','uni','busd','bch','link','sol','ltc','icp','wbtc','etc',
+    'matic','xlm','theta','vet','okb','fil','luna','dai','trx','cusd','aave','xmr','ceth','ftt','eos','cdai','atom','cake','cro',
+    'shib','grt','neo','klay','mkr','amp','bsv','xtz','leo','algo','cel','comp','AVAX','miota','btt','egld','steth','axs','ht',
+    'ust','ksm','hbar','qnt','rune','sushi','dcr','snx','dash','hbtc','waves','tfuel','xem','chz','zec','stx','safemoon','enj',
+    'hot','hnt','tusd','flow','yfi','zil','bcha','near','qtum','nexo','clout','bat','tel','btg','1inch','mana','xdc','bnt',
+    'pax','rvn','one','kcs','xsushi','mdx','sc','dgb']
+});*/
+
 async function getData() {
     const response = await fetch(url).then(response => {
         return response.json();
@@ -38,5 +57,10 @@ async function getData() {
 $(document).ready(function() {
     const btn = $('#search').click(function(event){
         getData();
+        $('#stat-box').css('display','block');
+        $('#pop-coins').css('display', 'none');
     });
+
+
+
 });
